45e22519f3e2ecc98e4ba22403ca5cacca93199d
Rogal <l_rogalski@hotmail.com>
2017-12-01 22:38:09 +0100

    Day_1/Part_1

--- Advent_of_code/Source.cpp
+++ Advent_of_code/Source.cpp
@@ -2,6 +2,20 @@
 
 int main()
 {
+	char t;
+	int sum = 0;
+	char temp = 0;
+	do
+	{
+		std::cin >> t;
+		if (t == temp)
+		{
+			sum += t - 48;
+		}
+		temp = t;
+	} while ( t != '.' );
+	std::cout << sum;
 
+	system("PAUSE");
 	return 0;
 }
