793439514e334b329f14274492b07f27f933359b
Rogal <l_rogalski@hotmail.com>
2017-12-09 00:03:53 +0100

    Day_2/Part_1

--- Advent_of_code/Source.cpp
+++ Advent_of_code/Source.cpp
@@ -1,32 +1,75 @@
 #include <iostream>
 #include <string>
+#include <fstream>
+
+int Find_Difference( std::string line );
 
 int main()
 {
-	std::string t;
+	std::string line;
 	int sum = 0;
-	char temp = 0;
-	std::cin >> t;
-	int half_len = t.length() / 2;
+	int n; std::cin >> n;
 
-	for (unsigned int i = 0; i < t.length(); ++i)
+	std::fstream file;
+	file.open("input.txt", std::ios::in);
+	
+	if (file.good() == false)
 	{
-		temp = t[half_len];
-		if (t[i] == temp)
+		std::cout << "File doesn't exist!";
+		exit(0);
+	}
+
+	while (getline(file, line))
+	{
+		for (int i = 0; i < n; ++i)
 		{
-			sum += t[i] - '0';
-		}
-		if (half_len == t.length() - 1)
-		{
-			half_len = 0;
-		}
-		else
-		{
-			++half_len;
+			//getline(std::cin, line);
+			sum += Find_Difference(line);
+			Find_Difference(line);
+			std::cout << Find_Difference(line) << std::endl;
 		}
 	}
-	std::cout << sum;
+	
+	file.close();
+	file.clear();
+
+	std::cout << sum << std::endl;
 
 	system("PAUSE");
 	return 0;
+}
+
+int Find_Difference( std::string line )
+{
+	int max, min, temp;
+	std::string segment;
+	max = min = std::stoi(line.substr(0,4));
+
+	for ( int i = 0; i < line.length(); ++i )
+	{
+		if ( line[i] == ' ' )
+			continue;
+
+		if ( line[i + 1] != ' ' )
+		{
+			segment = line[i];
+			for ( ; i < line.length(); ++i )
+			{
+				if ( line[i + 1] != ' ' )
+					segment += line[i + 1];
+				else
+					break;
+			}
+			temp = std::stoi(segment);
+		}
+		else
+			temp = line[i] - '0';
+
+		if ( max < temp )
+			max = temp;
+		if ( min > temp )
+			min = temp;
+	}
+
+	return max - min;
 }
